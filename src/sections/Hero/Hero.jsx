20de894b9e59d96f83dc7ef3338cbdb022d07f3a
mubson11 <mubson.karki+gitlab@innovatetech.co>
2024-09-18 17:18:56 +0545

    updates branches

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -1,32 +1,34 @@
-import styles from './HeroStyles.module.css';
-import heroImg from '../../assets/Design uten navn.png';
-import sun from '../../assets/sun.svg';
-import moon from '../../assets/moon.svg';
-import twitterLight from '../../assets/twitter-light.svg';
-import twitterDark from '../../assets/twitter-dark.svg';
-import githubLight from '../../assets/github-light.svg';
-import githubDark from '../../assets/github-dark.svg';
-import linkedinLight from '../../assets/linkedin-light.svg';
-import linkedinDark from '../../assets/linkedin-dark.svg';
-import CV from '../../assets/cv.pdf';
-import { useTheme } from '../../common/ThemeContext';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import styles from "./HeroStyles.module.css";
+import heroImg from "../../assets/Design uten navn.png";
+import sun from "../../assets/sun.svg";
+import moon from "../../assets/moon.svg";
+import githubLight from "../../assets/github-light.svg";
+import githubDark from "../../assets/github-dark.svg";
+import linkedinLight from "../../assets/linkedin-light.svg";
+import linkedinDark from "../../assets/linkedin-dark.svg";
+import CV from "../../assets/cv.pdf";
+import { useTheme } from "../../common/ThemeContext";
 
 function Hero() {
   const { theme, toggleTheme } = useTheme();
 
-  const themeIcon = theme === 'light' ? sun : moon;
-  const twitterIcon = theme === 'light' ? twitterLight : twitterDark;
-  const githubIcon = theme === 'light' ? githubLight : githubDark;
-  const linkedinIcon = theme === 'light' ? linkedinLight : linkedinDark;
+  const themeIcon = theme === "light" ? sun : moon;
+  const twitterIcon = (
+    <FontAwesomeIcon
+      icon={faEnvelope}
+      style={{ height: "44px", width: "44px", marginRight: "2px" }}
+      color={theme === "light" ? "black" : "white"}
+    />
+  );
+  const githubIcon = theme === "light" ? githubLight : githubDark;
+  const linkedinIcon = theme === "light" ? linkedinLight : linkedinDark;
 
   return (
     <section id="hero" className={styles.container}>
       <div className={styles.colorModeContainer}>
-        <img
-          src={heroImg}
-          className={styles.hero}
-          alt="Profile picture of Harris Johnsen"
-        />
+        <img src={heroImg} className={styles.hero} alt="Profile picture " />
         <img
           className={styles.colorMode}
           src={themeIcon}
@@ -36,14 +38,19 @@ function Hero() {
       </div>
       <div className={styles.info}>
         <h1>
-          Harris
+          Sagun
           <br />
-          Johnsen
+          Shrestha
         </h1>
-        <h2>Frontend Developer</h2>
+        <h2>QA/QC Engineer</h2>
         <span>
-          <a href="https://twitter.com/" target="_blank">
-            <img src={twitterIcon} alt="Twitter icon" />
+          <a
+            href={
+              "https://mail.google.com/mail/?view=cm&fs=1&tf=1&to=sagunshrestha24@outlook.com"
+            }
+            target="_blank"
+          >
+            {twitterIcon}
           </a>
           <a href="https://github.com/" target="_blank">
             <img src={githubIcon} alt="Github icon" />
@@ -53,8 +60,9 @@ function Hero() {
           </a>
         </span>
         <p className={styles.description}>
-          With a passion for developing modern React web apps for commercial
-          businesses.
+          QA/QC Engineer with a keen eye for detail, committed to upholding
+          quality standards and driving continuous improvement in software
+          development processes.
         </p>
         <a href={CV} download>
           <button className="hover">Resume</button>
